84fbfea5922f523844ef5e9680b9229ff9ba00fa
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-07-31 14:56:46 -0300

    feat: scrolltotop

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import RoutesApp from './routes';
 import { BrowserRouter } from 'react-router-dom';
 import { RecoilRoot } from 'recoil';
+import ScrollToTop from 'components/ScrollToTop';
 
 
 
@@ -13,6 +14,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <RecoilRoot>
         <RoutesApp />
       </RecoilRoot>
--- src/routes.tsx
+++ src/routes.tsx
@@ -4,12 +4,12 @@ import PaginaComum from 'pages/PaginaComum'
 import PaginaDeContato from 'pages/PaginaDeContato'
 import PaginaTecnologia from 'pages/PaginaTecnologia'
 import React from 'react'
-import {Routes, Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 
 
 function RoutesApp() {
   return (
-    <Routes>
+    <Routes>      
       <Route path='/' element={<PaginaComum />}>
         <Route index element={<Home />} />
         <Route path='tecnologia/:id' element={<PaginaTecnologia />} />
