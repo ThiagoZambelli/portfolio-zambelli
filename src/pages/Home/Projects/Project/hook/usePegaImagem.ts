9fcc75c3ebf9522fa86838bacdf683e8ee42aa7c
ThiagoZambelli <thiagozamoliveira@gmail.com>
2023-08-21 20:25:44 -0300

    teste de copilação

--- src/pages/Home/Projects/Project/hook/usePegaImagem.ts
+++ src/pages/Home/Projects/Project/hook/usePegaImagem.ts
@@ -1,12 +1,12 @@
-import img2 from '../../assets/2.svg'
-import img3 from '../../assets/3.svg'
-import img4 from '../../assets/4.svg'
-import img5 from '../../assets/5.svg'
-import img6 from '../../assets/6.svg'
-import img7 from '../../assets/7.svg'
-import img8 from '../../assets/8.svg'
-import img9 from '../../assets/9.svg'
-import img10 from '../../assets/10.svg'
+import img2 from '../../assets/2.png'
+import img3 from '../../assets/3.png'
+import img4 from '../../assets/4.png'
+import img5 from '../../assets/5.png'
+import img6 from '../../assets/6.png'
+import img7 from '../../assets/7.png'
+import img8 from '../../assets/8.png'
+import img9 from '../../assets/9.png'
+import img10 from '../../assets/10.png'
 
 export default function usePegaImagem () {
 
